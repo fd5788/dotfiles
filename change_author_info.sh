a6882d1d097936fc53b0c5e79bef439c05de27c1
fd5788 <dengzxf@gmail.com>
2019-06-14 13:41:22 +0800

    add tool for change author info

---
+++ change_author_info.sh
@@ -0,0 +1,12 @@
+#!sh
+#ref:https://help.github.com/en/articles/changing-author-info
+#execute following first, and then execute "git push --force --tags origin 'refs/heads/*'"
+
+git filter-branch --env-filter '
+CORRECT_NAME="fd5788"
+CORRECT_EMAIL="dengzxf@gmail.com"
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+' --tag-name-filter cat -- --branches --tags
