ae8d95c53692e614aa4e45830370690cb8b316a3
fd5788 <dengzxf@gmail.com>
2019-06-14 20:59:02 +0800

    filter-branch force

--- change_author_info.sh
+++ change_author_info.sh
@@ -1,8 +1,8 @@
-#!sh
+#!/bin/bash
 #ref:https://help.github.com/en/articles/changing-author-info
 #execute following first, and then execute "git push --force --tags origin 'refs/heads/*'"
 
-git filter-branch --env-filter '
+git filter-branch -f --env-filter '
 CORRECT_NAME="fd5788"
 CORRECT_EMAIL="dengzxf@gmail.com"
 export GIT_COMMITTER_NAME="$CORRECT_NAME"
